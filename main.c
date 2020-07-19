2e4c3e77120c323ab1c47ce7b097e3fa651f5df8
paul.cabrera <paul.cabrera@etu.u-bordeaux.fr>
2020-07-19 16:32:32 +0200

    main et Cmake

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void display_hello(void){
+    printf("Hello_World\n");
+    return;
+}
+
+int main(int argc, char * argv[]){
+    display_hello();
+    return 0;
+}
+
